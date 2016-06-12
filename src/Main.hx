890499601083c6af38fe26f02c33338153b746e5
kevinresol <kevinresol@gmail.com>
2016-06-13 00:41:17 +0800

    Minor

--- src/Main.hx
+++ src/Main.hx
@@ -2,28 +2,33 @@ package;
 
 import Sys.*;
 import sys.io.Process;
+using haxe.io.Path;
 
 class Main {
-	static var process:Process;
+	static var server:Process;
 	static function main() {
-		process = new Process('haxe', ['--wait', '6115']);
-		compile('tests', 'build.hxml');
+		server = new Process('haxe', ['--wait', '6115']);
+		compile('tests/build.hxml');
 		command('echo "" >> tests/MyTest.hx'); // make some modification
-		compile('tests', 'build.hxml');
-		process.kill();
-		trace('Test passed');
+		compile('tests/build.hxml');
+		server.kill();
+		println('Test passed');
 		exit(0);
 	}
 	
-	static function compile(cwd:String, hxml:String) {
+	static function compile(hxmlPath:String) {
+		println('Compiling $hxmlPath...');
+		var dir = hxmlPath.directory();
+		var hxml = hxmlPath.withoutDirectory();
 		var currentCwd = getCwd();
-		setCwd(cwd);
-		var exitCode = Sys.command('haxe', [hxml]);
+		setCwd(dir);
+		var exitCode = command('haxe', [hxml]);
 		if(exitCode != 0) {
-			trace('Test failed');
-			process.kill();
+			println('Compiled failed');
+			println('Test failed');
+			server.kill();
 			exit(exitCode);
-		}
+		} else println('Compiled $hxmlPath');
 		setCwd(currentCwd);
 	}
 }
